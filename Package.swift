d150a57641282aabcfe5db9d680baf445bfa6947
hyeon-k <hyeon.k@kakaomobility.com>
2024-04-15 17:06:34 +0900

    Revert "KNSDK 1.10.2"
    This reverts commit c912289cd57e782772a96546a3a542476ee7d657.

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@
 import PackageDescription
 
 let sdkName = "KNSDK"
-let version = "1.10.2"
+let version = "1.9.9"
 
 let package = Package(
     name: sdkName,
     platforms: [
-        .iOS(.v15)
+        .iOS(.v12)
     ],
     products: [
         .library(
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["KNSDKBundle"])
     ],
     dependencies: [
-        .package(url: "https://github.com/kakaomob/KMLocationSDK.git", exact: Version(stringLiteral: "1.103.71")),
+        .package(url: "https://github.com/kakaomob/KMLocationSDK.git", exact: Version(stringLiteral: "1.103.63")),
     ],
     targets: getRealmFromNexus(),
     swiftLanguageVersions: [.v5]
@@ -25,8 +25,8 @@ let package = Package(
 func getRealmFromNexus() -> [Target] {
     let targets: [Target]  = [
         .binaryTarget(name: sdkName,
-                      url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/1.10.2/KNSDK/KNSDK.xcframework.zip",
-                      checksum: "b2bf0b2232491fca4b6531a7094170e854f1aad6fe2e7da40934a08e88cbd427"),
+                      url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/1.9.9/KNSDK/KNSDK.xcframework.zip",
+                      checksum: "074b4f038bdbdc8bf91fbfba1ec0e4a8530261d3dfe43f068bf838e669e1d198"),
         .target(name: "KNSDKBundle",
                 dependencies: [
                     .target(name: sdkName),
